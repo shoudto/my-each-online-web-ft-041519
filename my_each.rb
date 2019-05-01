122712fc70b566a2554bfa4cba08dfe247f861ea
Oscar Shoudt <shoudto@gmail.com>
2019-05-01 16:37:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_each(array) # put argument(s) here
   counter = 0 
   
-  while i < array.length 
+  while counter < array.length 
     yield array[counter]
     counter += 1
   end 
- 
+ return array
 end 
