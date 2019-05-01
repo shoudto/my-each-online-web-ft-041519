561d31ba35fdcc599d25028227f3a8660acf3e1e
Oscar Shoudt <shoudto@gmail.com>
2019-05-01 15:48:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
-end
+require 'pry'
+
+def my_each(words) # put argument(s) here
+ # binding.pry
+   my_each(words) do |i|
+     puts i 
+  end 
+end 
